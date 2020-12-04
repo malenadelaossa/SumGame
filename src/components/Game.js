76b9350a7264006e0fd93fb97a0562bb4ae29e1a
Malena de la Ossa <malenadelaossa@hotmail.com>
2020-12-03 21:25:49 -0600

    Clase 5

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,21 +1,41 @@
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import React from 'react';
+import RandomNumber from './RandomNumber';
 
-export default Game = ({ randomNumbersCount }) => {
-  //const target = 10 + Math.floor(40 * Math.random());
-  const randomNumbers = Array.from({ length: randomNumbersCount })
-    .map(() => {
-      return  1 + Math.floor(9 * Math.random());
-    });
-  const target = randomNumbers.slice(0, randomNumbersCount - 2)
-    .reduce((acc, cur) => acc + cur, 0);
+export default Game = ({ randomNumbersCount = 4 }) => {
+  const [ selectedNumbers, setSelectedNumbers ] = useState([]);
+  const [ randomNumbers, setRandomNumbers ] = useState([]);
+  const [ target, setTarget ] = useState([]);
+
+  useEffect(() => {
+    const firstRandomNumbers = Array.from({ length: randomNumbersCount })
+      .map(() => 1 + Math.floor(10 * Math.random()));
+    const firstTarget = firstRandomNumbers.slice(0, randomNumbersCount - 2)
+      .reduce((acc, cur) => acc + cur, 0);
+    setRandomNumbers(firstRandomNumbers);
+    setTarget(firstTarget);
+  }, []);
+
+  const isNumberSelected = numberIndex =>
+    selectedNumbers.some(number => number === numberIndex);
+
+  const selectNumber = number =>
+    setSelectedNumbers([ ...selectedNumbers, number ]);
+
   return (
     <View>
       <Text style={styles.target}>{target}</Text>
-      {randomNumbers.map((randomNumber, index) =>
-        <Text key={index}>{randomNumber}</Text>
-      )}
+      <View style={styles.randomContainer}>
+        {randomNumbers.map((randomNumber, index) =>
+          <RandomNumber
+            id={index}
+            key={index}
+            number={randomNumber}
+            disabled={isNumberSelected(index)}
+            onSelected={selectNumber} />
+        )}
+      </View>
     </View>
   );
 };
@@ -25,5 +45,11 @@ const styles = StyleSheet.create({
     fontSize: 40,
     backgroundColor: '#aaa',
     textAlign: 'center',
+  },
+  randomContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
   },
 });
